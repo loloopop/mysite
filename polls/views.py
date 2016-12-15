b03874a5f58293648a2a5c8cd5a6b18f1831e1be
yueliqiang <loloopop@126.com>
2016-12-15 17:00:42 +0800

    git记录文件

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,10 @@
 # -*-coding:utf-8 -*-
 import sys
-from django.http import HttpResponse
-from django.shortcuts import render
-from .models import Question
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.core.urlresolvers import reverse
+
+from .models import Question, Choice
 
 # 以下为防止中文字符乱码
 # ----------------------------------------------
@@ -14,18 +16,29 @@ if sys.getdefaultencoding() != default_encoding:
 # ----------------------------------------------
 # 2016年12月14日14:52:38 更改
 
-
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("问题编号 %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "结果编号 %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("处理编号 %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html'), {
+            'question': question,
+            'error_message': "您未选择任何项目",
+        }
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
